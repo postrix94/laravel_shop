cc94a8505d11d6eaa8f64d96b4098a7dab59bf6a
unknown <postrix94@gmail.com>
2023-07-12 22:45:15 +0300

    Categories CRUD

--- app/Http/Requests/Category/CreateCategoryRequest.php
+++ app/Http/Requests/Category/CreateCategoryRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateCategoryRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = true;
     /**
      * Determine if the user is authorized to make this request.
      */
